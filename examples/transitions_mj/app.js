044922ce1b50e58fdb83b5c7308dee7ac342b85d
Clément Noterdaem <clement@misterjekyll.be>
2015-02-25 15:29:22 +0100

    base routes

--- examples/transitions_mj/app.js
+++ examples/transitions_mj/app.js
@@ -1,31 +1,69 @@
 
-// dom elements
-var content = document.querySelector('#content');
-var p = document.querySelector('#page');
+// SYSTEM
+(function () {
+
+    var cache = {};
+
+    function get(url, cb) {
+        if (cache[url]) return cb(cache[url]);
+        $.ajax({
+            url: url,
+            success: function(data) {
+                cache[url] = data;
+                cb(data);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR, textStatus, errorThrown);
+            },
+            dataType: 'text'
+        });
+    }
+
+    function view(html, data, partials) {
+        var template = Hogan.compile(html),
+            content = template.render(data, partials);
+        $('#content').empty().append(content);
+    }
+
+    window.init = {
+        ctx: function (ctx, next) {
+            ctx.data = {};
+            ctx.partials = {};
+            if (ctx.init) {
+                next();
+            } else {
+                $('#content').addClass('transition');
+                setTimeout(function(){
+                    $('#content').removeClass('transition');
+                    next();
+                }, 300);
+            }
+        }
+    };
+
+    window.render = {
+        page: function(ctx, next) {
+            var tp = ctx.path;
+            tp = tp.split("?")[0];
+            if (tp=='/') tp = '/home';
+            get('views'+tp+'.html', function(html) {
+                view(html, ctx.data, ctx.partials);
+            });
+        },
+        error: function(ctx, next) {
+            get('views/404.html', function(html) {
+                view(html, ctx.data, ctx.partials);
+            });
+        }
+    };
+
+    window.done = null;
+
+}());
 
 // ROUTES
-page.base('');
-page('/', index);
-page('/about', about);
-page('/contact', contact);
-page('*', notfound);
+page('*', init.ctx);
+page('/', render.page);
+page('/about', render.page);
+page('*', render.error);
 page();
-
-// PAGES
-function index() {
-    p.textContent = 'viewing index';
-}
-
-function about() {
-    p.textContent = 'viewing about';
-}
-
-function contact() {
-    p.textContent = 'viewing contact';
-}
-
-function notfound() {
-    p.textContent = 'not found';
-}
-
-
